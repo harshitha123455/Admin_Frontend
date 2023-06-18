105a9acf58fb76c2692ab197c1bb2c4553886162
JestinSabu <jestinsabu721@gmail.com>
2023-06-18 19:11:52 +0530

    Fix Timetable

--- src/pages/ViewTimeTable.js
+++ src/pages/ViewTimeTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Form, DatePicker, Select, Button, message } from "antd";
 import styled from "styled-components";
 import AdminService from "../services/admin-services";
+import moment from "moment";
 
 const ViewTable = () => {
   const [form] = Form.useForm();
@@ -17,19 +18,33 @@ const ViewTable = () => {
   }, []);
 
   const handleFormSubmit = async (values) => {
-    const selectedDate = moment(values.date).format("YYYY-MM-DD");
-    console.log("Selected Date:", selectedDate);
     console.log("Form submitted:", values);
 
-    // For testing purposes, provide a static timetable data
-    const staticTimetable = [
-      { id: 1, date: selectedDate, screen: "Screen 1", slot1: "9:00 AM", slot2: "12:00 PM", slot3: "3:00 PM", slot4: "6:00 PM" },
-      { id: 2, date: selectedDate, screen: "Screen 2", slot1: "10:00 AM", slot2: "1:00 PM", slot3: "4:00 PM", slot4: "7:00 PM" },
-      { id: 3, date: selectedDate, screen: "Screen 3", slot1: "11:00 AM", slot2: "2:00 PM", slot3: "5:00 PM", slot4: "8:00 PM" },
-    ];
+    const response = await adminService.getTimeTable(values.date, values.screen);
+    if(response[0]){
+      const slotObjects = {
+        slot1: response[1].slot1,
+        slot2: response[1].slot2,
+        slot3: response[1].slot3,
+        slot4: response[1].slot4,
+      }
+      const data = Object.values(slotObjects)
+      message.success("Timetable retrieved");
+      setTimetable(data);
+      console.log(data);
+    }
+    else{
+      message.error("Timetable not found");
+    }
 
-    setTimetable(staticTimetable);
-    message.success("Timetable retrieved");
+    // For testing purposes, provide a static timetable data
+    // const staticTimetable = [
+    //   { id: 1, date: "2000-01-01", screen: "Screen 1", slot1: "9:00 AM", slot2: "12:00 PM", slot3: "3:00 PM", slot4: "6:00 PM" },
+    //   { id: 2, date: "2000-01-01", screen: "Screen 2", slot1: "10:00 AM", slot2: "1:00 PM", slot3: "4:00 PM", slot4: "7:00 PM" },
+    //   { id: 3, date: "2000-01-01", screen: "Screen 3", slot1: "11:00 AM", slot2: "2:00 PM", slot3: "5:00 PM", slot4: "8:00 PM" },
+    // ];
+
+    // setTimetable(staticTimetable);
   };
 
   return (
@@ -62,23 +77,21 @@ const ViewTable = () => {
             <Table>
               <thead>
                 <tr>
-                  <th>Date</th>
+                  <th>Time</th>
                   <th>Screen</th>
-                  <th>Slot 1</th>
-                  <th>Slot 2</th>
-                  <th>Slot 3</th>
-                  <th>Slot 4</th>
+                  <th>Available Normal Seats</th>
+                  <th>Available Executive Seats</th>
+                  <th>Available Premium Seats</th>
                 </tr>
               </thead>
               <tbody>
                 {timetable.map((entry) => (
                   <tr key={entry.id}>
-                    <td>{entry.date}</td>
-                    <td>{entry.screen}</td>
-                    <td>{entry.slot1}</td>
-                    <td>{entry.slot2}</td>
-                    <td>{entry.slot3}</td>
-                    <td>{entry.slot4}</td>
+                    <td>{entry.time}</td>
+                    <td>{entry.movie.name}</td>
+                    <td>{entry.seatingArrangement.availableNormalSeats}</td>
+                    <td>{entry.seatingArrangement.availablePremiumSeats}</td>
+                    <td>{entry.seatingArrangement.availableExecutiveSeats}</td>
                   </tr>
                 ))}
               </tbody>
